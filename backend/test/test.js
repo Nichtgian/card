a6892708780290ad63ba3f00f868156e26f8130e
Nychtgian <nichtgian@gmail.com>
2019-06-25 20:47:36 +0200

    Refactored tests

--- backend/test/test.js
+++ backend/test/test.js
@@ -2,44 +2,36 @@ class TestResult {
   constructor(testSetup, testCase) {
     this.passed = true;
     this.result = null;
-    this.expectedResult = testCase.expectedResult;
-    this.stack = testSetup.stack.toString();
-    this.card = testCase.card;
+    this.test = {
+      setup: testSetup,
+      case: testCase
+    }
   }
 }
 
 runAllTests();
 function runAllTests() {
-  console.table(setupTest_ShitHead_canCardBePlayed());
-
-  let results = setupTest_ShitHead_canCardBePlayed();
-  let unit = "test";
-
-  console.groupCollapsed('Testing unit: %s', unit);
-  for (var r in results) {
-      var res = results[r];
-      console.groupCollapsed('%c  %c' + res.passed, 'background-color: ' + (res.passed == 'false' ? 'red' : 'green') + '; margin-right: 10px', 'background-color: transparent');
-      console.table({
-          Result:   {value: res.result},
-          Function: {value: res.expectedResult},
-          Asserted: {value: res.stack},
-          Returned: {value: res.card}
-      });
-      console.groupEnd();
-  }
-  console.groupEnd();
-
+  writeTestResults(setupTest_ShitHead_canCardBePlayed());
   setTimeout(() => {
     console.clear();
-  }, 20000);
+  }, 120000);
 }
 
-function writeTestResult(testResult, testCase) {
-  if (!testResult.passed) {
-    console.error("failed! received(" + testResult.result + ") expected(" + testCase.expectedResult + ")");
-  } else {
-    console.log("passed!");
-  }
+function writeTestResults(testResults) {
+  console.groupCollapsed('Test ShitHead_canCardBePlayed');
+  testResults.forEach((result) => {
+    console.groupCollapsed('%c__%c' + (result.passed == false ? ' failed' : ' passed'),
+      'background-color: ' + (result.passed == false ? 'red' : 'green'),
+      'background-color: transparent');
+    console.table({
+        Result: { value: result.result },
+        Expected: { value: result.test.case.expectedResult },
+        TestSetup: { value: result.test.setup },
+        TestCase: { value: result.test.case }
+    });
+    console.groupEnd();
+  });
+  console.groupEnd();
 }
 
 function setupTest_ShitHead_canCardBePlayed() {
@@ -49,7 +41,7 @@ function setupTest_ShitHead_canCardBePlayed() {
     },
     testCases: [
       { card: '2', expectedResult: true },
-      { card: '3', expectedResult: true },
+      { card: '3', expectedResult: false },
       { card: '4', expectedResult: false },
       { card: '5', expectedResult: false },
       { card: '7', expectedResult: true },
@@ -98,9 +90,7 @@ function setupTest_ShitHead_canCardBePlayed() {
 
 function runTest_ShitHead_canCardBePlayed(testSetup, testCase) {
   let testResult = new TestResult(testSetup, testCase);
-
   testResult.result = new ShitHead().isCardPlayable(testCase.card, testSetup.stack);
-
   if (testResult.result != testCase.expectedResult) {
     testResult.passed = false;
   }
